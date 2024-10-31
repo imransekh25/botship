35bbdc73097dd580de2c2dd4111a0db2536fda99
imran.sekh <imransekh25@gmail.com>
2024-10-31 21:42:22 +0530

    New Application For Global Shipping Delivery management

---
+++ src/main/java/com/botbel/botship/BotshipApplication.java
@@ -0,0 +1,13 @@
+package com.botbel.botship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botbel/botship/BotshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botbel.botship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
